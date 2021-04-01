9b3c3ce0dd808f24ea7a14c17142df9088497998
MeLike2D <ml2dofficial@gmail.com>
2021-04-01 07:44:00 -0700

    🐛 fix resuming timeout not removing the client

--- Server/src/main/kotlin/obsidian/server/io/MagmaClient.kt
+++ Server/src/main/kotlin/obsidian/server/io/MagmaClient.kt
@@ -200,7 +200,7 @@ class MagmaClient(
 
       val runnable = Runnable {
         runBlocking {
-          this@MagmaClient.shutdown()
+          magma.shutdown(this@MagmaClient)
         }
       }
 
