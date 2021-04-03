67f002e8197b69d14943f574d79f399ffa071bf6
MeLike2D <ml2dofficial@gmail.com>
2021-04-02 21:57:50 -0700

    ✨ use extensions instead of a class

--- Server/src/main/kotlin/obsidian/server/io/controllers/Tracks.kt
+++ Server/src/main/kotlin/obsidian/server/io/controllers/Tracks.kt
@@ -41,137 +41,128 @@ import obsidian.server.util.TrackUtil
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class Tracks(routing: Routing) {
-  init {
-    routing {
-      authenticate {
-        @Location("/loadtracks")
-        data class LoadTracks(val identifier: String)
+private val logger: Logger = LoggerFactory.getLogger("TracksController")
 
-        get<LoadTracks> {
-          val response = loadTracks(it.identifier)
-          context.respond(response)
-        }
+fun Routing.tracks() {
+  authenticate {
+    @Location("/loadtracks")
+    data class LoadTracks(val identifier: String)
 
-        @Location("/decodetrack")
-        data class DecodeTrack(val track: String)
+    get<LoadTracks> { data ->
+      val result = AudioLoader(Obsidian.playerManager)
+        .load(data.identifier)
+        .await()
 
-        get<DecodeTrack> {
-          val track = TrackUtil.decode(it.track)
-          context.respond(getTrackInfo(track))
-        }
-
-        post("/decodetracks") {
-          val body = call.receive<DecodeTracksBody>()
-          context.respond(body.tracks.map(::getTrackInfo))
-        }
+      if (result.exception != null) {
+        logger.error("Track loading failed", result.exception)
       }
-    }
-  }
 
-  private suspend fun loadTracks(identifier: String): LoadTracksResponse {
-    val result = AudioLoader(Obsidian.playerManager)
-      .load(identifier)
-      .await()
+      val playlist = result.playlistName?.let {
+        LoadTracksResponse.PlaylistInfo(name = it, selectedTrack = result.selectedTrack)
+      }
 
-    if (result.exception != null) {
-      logger.error("Track loading failed", result.exception)
-    }
-
-    val playlist = result.playlistName?.let {
-      LoadTracksResponse.PlaylistInfo(name = it, selectedTrack = result.selectedTrack)
-    }
-
-    val exception = if (result.loadResultType == LoadType.LOAD_FAILED && result.exception != null) {
-      LoadTracksResponse.Exception(
-        message = result.exception!!.localizedMessage,
-        severity = result.exception!!.severity
-      )
-    } else {
-      null
-    }
-
-    return LoadTracksResponse(
-      tracks = result.tracks.map(::getTrack),
-      type = result.loadResultType,
-      playlistInfo = playlist,
-      exception = exception
-    )
-  }
-
-  private fun getTrack(audioTrack: AudioTrack): Track =
-    Track(track = audioTrack, info = getTrackInfo(audioTrack))
-
-  private fun getTrackInfo(audioTrack: AudioTrack): Track.Info =
-    Track.Info(
-      title = audioTrack.info.title,
-      uri = audioTrack.info.uri,
-      identifier = audioTrack.info.identifier,
-      author = audioTrack.info.author,
-      length = audioTrack.duration,
-      isSeekable = audioTrack.isSeekable,
-      isStream = audioTrack.info.isStream,
-      position = audioTrack.position
-    )
-
-  @Serializable
-  data class DecodeTracksBody(@Serializable(with = AudioTrackListSerializer::class) val tracks: List<AudioTrack>)
-
-  @Serializable
-  data class LoadTracksResponse(
-    @SerialName("load_type")
-    val type: LoadType,
-    @SerialName("playlist_info")
-    val playlistInfo: PlaylistInfo?,
-    val tracks: List<Track>,
-    val exception: Exception?
-  ) {
-    @Serializable
-    data class Exception(
-      val message: String,
-      val severity: FriendlyException.Severity
-    )
-
-    @Serializable
-    data class PlaylistInfo(
-      val name: String,
-      @SerialName("selected_track")
-      val selectedTrack: Int?
-    )
-  }
-
-  @Serializable
-  data class Track(
-    @Serializable(with = AudioTrackSerializer::class)
-    val track: AudioTrack,
-    val info: Info
-  ) {
-    @Serializable
-    data class Info(
-      val title: String,
-      val author: String,
-      val uri: String,
-      val identifier: String,
-      val length: Long,
-      val position: Long,
-      @SerialName("is_stream")
-      val isStream: Boolean,
-      @SerialName("is_seekable")
-      val isSeekable: Boolean,
-    )
-  }
-
-  // taken from docs lmao
-  object AudioTrackListSerializer : JsonTransformingSerializer<List<AudioTrack>>(ListSerializer(AudioTrackSerializer)) {
-    override fun transformDeserialize(element: JsonElement): JsonElement =
-      if (element !is JsonArray) {
-        JsonArray(listOf(element))
+      val exception = if (result.loadResultType == LoadType.LOAD_FAILED && result.exception != null) {
+        LoadTracksResponse.Exception(
+          message = result.exception!!.localizedMessage,
+          severity = result.exception!!.severity
+        )
       } else {
-        element
+        null
       }
-  }
 
-  companion object {
-    private val logger: Logger = LoggerFactory.getLogger(Tracks::class.java)
+      val response = LoadTracksResponse(
+        tracks = result.tracks.map(::getTrack),
+        type = result.loadResultType,
+        playlistInfo = playlist,
+        exception = exception
+      )
+
+      context.respond(response)
+    }
+
+    @Location("/decodetrack")
+    data class DecodeTrack(val track: String)
+
+    get<DecodeTrack> {
+      val track = TrackUtil.decode(it.track)
+      context.respond(getTrackInfo(track))
+    }
+
+    post("/decodetracks") {
+      val body = call.receive<DecodeTracksBody>()
+      context.respond(body.tracks.map(::getTrackInfo))
+    }
   }
+}
+
+private fun getTrack(audioTrack: AudioTrack): Track =
+  Track(track = audioTrack, info = getTrackInfo(audioTrack))
+
+private fun getTrackInfo(audioTrack: AudioTrack): Track.Info =
+  Track.Info(
+    title = audioTrack.info.title,
+    uri = audioTrack.info.uri,
+    identifier = audioTrack.info.identifier,
+    author = audioTrack.info.author,
+    length = audioTrack.duration,
+    isSeekable = audioTrack.isSeekable,
+    isStream = audioTrack.info.isStream,
+    position = audioTrack.position
+  )
+
+@Serializable
+data class DecodeTracksBody(@Serializable(with = AudioTrackListSerializer::class) val tracks: List<AudioTrack>)
+
+@Serializable
+data class LoadTracksResponse(
+  @SerialName("load_type")
+  val type: LoadType,
+  @SerialName("playlist_info")
+  val playlistInfo: PlaylistInfo?,
+  val tracks: List<Track>,
+  val exception: Exception?
+) {
+  @Serializable
+  data class Exception(
+    val message: String,
+    val severity: FriendlyException.Severity
+  )
+
+  @Serializable
+  data class PlaylistInfo(
+    val name: String,
+    @SerialName("selected_track")
+    val selectedTrack: Int?
+  )
+}
+
+@Serializable
+data class Track(
+  @Serializable(with = AudioTrackSerializer::class)
+  val track: AudioTrack,
+  val info: Info
+) {
+  @Serializable
+  data class Info(
+    val title: String,
+    val author: String,
+    val uri: String,
+    val identifier: String,
+    val length: Long,
+    val position: Long,
+    @SerialName("is_stream")
+    val isStream: Boolean,
+    @SerialName("is_seekable")
+    val isSeekable: Boolean,
+  )
+}
+
+// taken from docs lmao
+object AudioTrackListSerializer : JsonTransformingSerializer<List<AudioTrack>>(ListSerializer(AudioTrackSerializer)) {
+  override fun transformDeserialize(element: JsonElement): JsonElement =
+    if (element !is JsonArray) {
+      JsonArray(listOf(element))
+    } else {
+      element
+    }
 }
